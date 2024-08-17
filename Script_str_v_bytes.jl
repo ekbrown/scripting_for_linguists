25a14ae889e5e4ce8c090d16bea42fa30edb374e
Earl Brown <ekb5@BrownEarl2-5.local>
2024-08-17 16:19:05 -0600

    str v bytes

--- Script_str_v_bytes.jl
+++ Script_str_v_bytes.jl
@@ -27,17 +27,27 @@ out_path = "/pathway/to/times_jl.csv"
 txt = open(in_path) do infile
     uppercase(read(infile, String))
 end
+
+### strings ###
 wds = split(txt)
+
+### bytes; thanks to @andrebieler7906 for his comment on https://youtu.be/tvLpiDIQMjQ
+bwds = [Vector{UInt8}(word) for word in wds]
+
 println("number of words = ", length(wds))
 println(typeof(wds))
 open(out_path, "w") do outfile
-    write(outfile, "n_wds,lang,sec\n")
+    write(outfile, "n_wds,lang,sec_str,sec_bytes\n")
     for i in 50:50:10000
         println(i)
         t1= time_ns()
-        mattr = get_mattr(wds[1:i])
-        dur_jl = (time_ns()-t1)/1_000_000_000
-        println("Julia took ", dur_jl, " and got ", mattr)
-        write(outfile, "$i,Julia,$dur_jl\n")        
+        mattr_str = get_mattr(wds[1:i]) # strings
+        dur_jl_str = (time_ns()-t1)/1_000_000_000
+        t2=time_ns()
+        mattr_bytes = get_mattr(@view bwds[1:i]) # bytes
+        dur_jl_bytes = (time_ns()-t2)/1_000_000_000
+        println("MATTR (str)   = ", mattr_str)
+        println("MATTR (bytes) = ", mattr_bytes)
+        write(outfile, "$i,Julia,$dur_jl_str,$dur_jl_bytes\n")        
     end
 end
